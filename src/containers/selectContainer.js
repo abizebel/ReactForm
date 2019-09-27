8172f95871ec46fabe6df7ac190761740dfb5f96
Abbas <abizebel@gmail.com>
2019-09-27 20:18:04 +0330

    .

--- src/containers/selectContainer.js
+++ src/containers/selectContainer.js
@@ -12,12 +12,11 @@ class SelectContainer extends Component {
       <Fragment>
         
         <div className="page-content-box" >
-          <div className="page-content-title">Select</div>
+          <div className="page-content-title">Multi Select</div>
           <Select   change={this.changeSelect.bind(this)}       
             label={'Last Name'}
-            defaultValue ={11}
+            defaultValue ={[11,22,33]}
             multi={true}
-            showKey ={true}
             nullable={true}
             search = {true}
             searchLabel = {'Search ... '}
@@ -33,10 +32,15 @@ class SelectContainer extends Component {
             ]}
             mapping = {{text : 'name', value : 'id'}}
           />
-          {/* <Select   change={this.changeSelect.bind(this)}
+
+          <Select   change={this.changeSelect.bind(this)}       
+            label={'نام خانوادگی'}            
+            defaultValue ={[11,22,33]}
+            multi={true}
             rtl={true}
-            label={'نام خانوادگی'}
-            defaultValue ={33}
+            nullable={true}
+            search = {true}
+            searchLabel = {'جستجو ...'}
             values ={
               [{id:'11',name:'حسینی' , info:{icon:sampleIcon}},
               {id:'22',name:'فیض', info:{icon:sampleIcon}},
@@ -46,6 +50,52 @@ class SelectContainer extends Component {
             ]}
             mapping = {{text : 'name', value : 'id'}}
           />
+
+
+        </div>
+        <div className="page-content-box" >
+          <div className="page-content-title">Outlined Multi Select</div>
+          <Select   change={this.changeSelect.bind(this)}       
+            label={'Last Name'}
+            defaultValue ={[11,22,33]}
+            multi={true}
+            outline={true}
+            nullable={true}
+            search = {true}
+            searchLabel = {'Search ... '}
+            values ={
+              [{id:'11',name:'Hosseini' , info:{icon:sampleIcon}},
+              {id:'22',name:'feiz', info:{icon:sampleIcon}},
+              {id:'33',name:'mohammadi', info:{icon:sampleIcon}},
+              {id:'44',name:'khosravi', info:{icon:sampleIcon}},
+              {id:'55',name:'ranjbar', info:{icon:sampleIcon}},
+              {id:'66',name:'aghapour', info:{icon:sampleIcon}},
+              {id:'77',name:'mardanian', info:{icon:sampleIcon}},
+              {id:'88',name:'babaei', info:{icon:sampleIcon}}
+            ]}
+            mapping = {{text : 'name', value : 'id'}}
+          />
+
+          <Select   change={this.changeSelect.bind(this)}       
+            label={'نام خانوادگی'}            
+            defaultValue ={[11,22,33]}
+            multi={true}
+            rtl={true}
+            outline={true}
+
+            nullable={true}
+            search = {true}
+            searchLabel = {'جستجو ...'}
+            values ={
+              [{id:'11',name:'حسینی' , info:{icon:sampleIcon}},
+              {id:'22',name:'فیض', info:{icon:sampleIcon}},
+              {id:'33',name:'محمدی', info:{icon:sampleIcon}},
+              {id:'44',name:'خسروی', info:{icon:sampleIcon}},
+              {id:'55',name:'رنجبر', info:{icon:sampleIcon}}
+            ]}
+            mapping = {{text : 'name', value : 'id'}}
+          />
+
 
         </div>
 
@@ -352,7 +402,7 @@ class SelectContainer extends Component {
               {id:'55',name:'رنجبر', info:{icon:sampleIcon}}
             ]}
             mapping = {{text : 'name', value : 'id'}}
-          />  */}
+          /> 
 
         </div>
 
