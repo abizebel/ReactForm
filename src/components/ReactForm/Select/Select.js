4a5a8227f62fb287748bbf041d42224945c39b30
Abbas Hosseini <abizebel@gmail.com>
2020-05-26 12:24:22 +0430

    fix

--- src/components/ReactForm/Select/Select.js
+++ src/components/ReactForm/Select/Select.js
@@ -193,11 +193,12 @@ class Select extends Component {
 
 
 
+
     setPosition = () => {
         const {outline} = this.props;
 
         this.setState({
-            top : $(this.inputDom.current).offset().top+ (outline ? 45: 30),
+            top : $(this.inputDom.current).offset().top+ (outline ? 44: 30),
             left : $(this.inputDom.current).offset().left,
             width : $(this.inputDom.current).parent().width()
         });
